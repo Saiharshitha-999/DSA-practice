c0d135c6df824dc96404178d3d49e8579f1ae562
Sai Harshita <saiharshitagit@gmail.com>
2025-12-17 19:20:09 +0530

    Add Maximum Consecutive Ones in the array Solution

---
+++ Arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class MaxConsecutiveOnes
+{
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==1)
+            {
+                count+=1;
+                max=Math.max(max,count);
+            }
+            else
+            {
+                count=0;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        MaxConsecutiveOnes obj=new MaxConsecutiveOnes();
+        int[] nums={1,1,0,1,1,1};
+        System.out.println(obj.findMaxConsecutiveOnes(nums));
+    }
+}
