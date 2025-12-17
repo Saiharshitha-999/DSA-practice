85318e54b4af00eac8024948700486cb39b5d566
Sai Harshita <saiharshitagit@gmail.com>
2025-12-17 15:12:46 +0530

    Add move zeroes to end Solution

---
+++ Arrays/MoveZeroestoend.java
@@ -0,0 +1,37 @@
+package Arrays;
+public class MoveZeroestoend {
+    public void moveZeroes(int[] nums) {
+        int i,j;
+        for(i=0;i<nums.length;i++)
+        {
+            if(nums[i]==0)
+            {
+                
+                break;
+            }
+        }
+        for(j=i+1;j<nums.length;j++)
+        {
+            if(nums[j]!=0)
+            {
+                int temp=nums[j];
+                nums[j]=nums[i];
+                nums[i]=temp;
+                i++;
+            }
+        
+        }
+    }
+    
+    public static void main(String[] args) {
+        MoveZeroestoend mz=new MoveZeroestoend();
+        int arr[]= {0,1,0,3,12};
+        mz.moveZeroes(arr);
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    
+}
+}
+
